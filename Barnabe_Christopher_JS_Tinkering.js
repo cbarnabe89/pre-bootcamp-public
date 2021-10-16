2f6e3c8a726d9ed62f710903908cc93bc11c38c9
cbarnabe89 <91102393+cbarnabe89@users.noreply.github.com>
2021-10-16 17:30:39 -0400

    Add files via upload

---
+++ Barnabe_Christopher_JS_Tinkering.js
@@ -0,0 +1,11 @@
+var childHeight = 32;
+function displayIfChildIsAbleToRideTheRollerCoaster() {
+    if (childHeight > 52) {
+        console.log("Get on that ride kiddo!");
+    }
+    else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+}
+var example = displayIfChildIsAbleToRideTheRollerCoaster();
+console.log (example)
